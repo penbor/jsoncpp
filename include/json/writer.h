f7b0e8963e530419586ba2b7e037761627fea780
Christopher Dunn <cdunn2001@gmail.com>
2015-01-29 11:49:37 -0600

    Revert "added option to FastWriter which omits the trailing new line character"
    This reverts commit 5bf16105b5a90f9fa7d5d4d43452feee46603a52.

--- include/json/writer.h
+++ include/json/writer.h
@@ -201,8 +201,6 @@ public:
    */
   void dropNullPlaceholders();
 
-  void omitEndingLineFeed();
-
 public: // overridden from Writer
   virtual std::string write(const Value& root);
 
@@ -212,7 +210,6 @@ private:
   std::string document_;
   bool yamlCompatiblityEnabled_;
   bool dropNullPlaceholders_;
-  bool omitEndingLineFeed_;
 };
 
 /** \brief Outputs a Value in <a HREF="http://www.json.org">JSON</a> format
